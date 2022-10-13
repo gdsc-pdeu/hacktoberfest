b6daef23858351a08abdab8afa9f26baf432fc77
Priya Srivastava <shivikapriya730@gmail.com>
2022-10-14 00:09:58 +0530

    Updates

---
+++ precache-manifest.375c5b3118342447c526cb6bc49a9c70.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148a7c69d1996c60b8d283e3779c963f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6692660e89e537b816c",
+    "url": "./static/css/main.83fcf903.chunk.css"
+  },
+  {
+    "revision": "a44184a43cabc26f67f9",
+    "url": "./static/js/2.f7cce959.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "./static/js/2.f7cce959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6692660e89e537b816c",
+    "url": "./static/js/main.6669725e.chunk.js"
+  },
+  {
+    "revision": "d643efd86317b00f2a9c",
+    "url": "./static/js/runtime-main.516770c3.js"
+  },
+  {
+    "revision": "305e3f04ae871152b9ccaaccb36402ff",
+    "url": "./static/media/attendance.305e3f04.jpg"
+  },
+  {
+    "revision": "09c664938b9de9c7695903cc126fbdbc",
+    "url": "./static/media/code.09c66493.jpg"
+  },
+  {
+    "revision": "c3f0581c9587774422e477f3be0f1237",
+    "url": "./static/media/contribute.c3f0581c.jpeg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "./static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "cd755f766f54308facf5af361c2c99e3",
+    "url": "./static/media/expense.cd755f76.jpg"
+  },
+  {
+    "revision": "fc98956be63d7330146199177ac421ac",
+    "url": "./static/media/faceDetection.fc98956b.jpg"
+  },
+  {
+    "revision": "2dd1afcee19807941e7b11c63f9d992d",
+    "url": "./static/media/fashion.2dd1afce.jpg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "./static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "./static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "./static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "./static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "./static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "./static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "75fc3d88b1e6e322b3478fca9e0da4da",
+    "url": "./static/media/healthiher.75fc3d88.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "./static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "./static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "783f8c645dfb8da98a8b5e53ed1eefbb",
+    "url": "./static/media/merge.783f8c64.png"
+  },
+  {
+    "revision": "6673f24d7fcc994430f89d51bb8a2bea",
+    "url": "./static/media/pioneer.6673f24d.jpg"
+  },
+  {
+    "revision": "66f8fca36a296779174a9977505f456e",
+    "url": "./static/media/quiz.66f8fca3.jpg"
+  },
+  {
+    "revision": "b57e38ce05a8108a484526334f7d659a",
+    "url": "./static/media/register.b57e38ce.jpeg"
+  },
+  {
+    "revision": "822d9c04ebad060a374ef83cef436acd",
+    "url": "./static/media/socialmedia.822d9c04.jpg"
+  },
+  {
+    "revision": "fbab2664be64b2f2efe9817d8fa1f349",
+    "url": "./static/media/truth.fbab2664.jpg"
+  },
+  {
+    "revision": "d0ff01e428bb1de9a2bff6f49ac1b84b",
+    "url": "./static/media/unipass.d0ff01e4.jpg"
+  }
+]);
